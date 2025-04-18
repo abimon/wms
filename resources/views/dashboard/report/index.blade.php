510471a1a8c3f64a61ed31dcb27f80497add0e3f
abimon <eddyabby59@gmail.com>
2025-04-18 13:16:01 +0300

    general reports manager

--- resources/views/dashboard/report/index.blade.php
+++ resources/views/dashboard/report/index.blade.php
@@ -1,30 +1,46 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="table table-responsive">
-        <table>
-            <thead>
-                <th>#</th>
-                <th>Contact</th>
-                <th>Vehicle</th>
-                <th>Latitude</th>
-                <th>Longitude</th>
-                <th>Description</th>
-            </thead>
-            <tbody>
-                @foreach($reports as $report)
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $report->contact }}</td>
-                    <td>{{ $report->vehicle }}</td>
-                    <td>{{ $report->latitude }}</td>
-                    <td>{{ $report->longitude }}</td>
-                    <td>{{ $report->description }}</td>
-                    <td>{{ date_format($report->created_at, 'F jS, Y H:i:s') }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+    <div class="container">
+        <div class="table table-responsive">
+            <table>
+                <thead>
+                    <th>#</th>
+                    <th>Contact</th>
+                    <th>Vehicle</th>
+                    <th>Location</th>
+                    <th>Description</th>
+                    <th>Report Time</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>
+                    @foreach($reports as $report)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $report->contact }}</td>
+                            <td>{{ $report->vehicle }}</td>
+                            <td>
+                                <a href="https://maps.google.com/maps?q={{ ($report->latitude) . ',' . ($report->longitude) }}"
+                                    target="_blank" title="Click to view on map">
+                                    {{ ($report->latitude) . ' ' . ($report->longitude) }}
+                                </a>
+                            </td>
+                            <td>{{ $report->description }}</td>
+                            <td>{{ date_format($report->created_at, 'F jS, Y H:i:s') }}</td>
+                            <td>
+                                <!-- <a href="{{ route('report.show', $report->id) }}" class="btn btn-primary">View</a>
+                                        <a href="{{ route('report.edit', $report->id) }}" class="btn btn-warning">Edit</a> -->
+                                <form action="{{ route('report.destroy', $report->id) }}" method="POST" class="d-inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger">Delete</button>
+                                </form>
+                            </td>
+                            </td>
+
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
 @endsection
