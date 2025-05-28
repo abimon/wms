fb6490f7083e69e72da1b8da8fbac733f03e0cef
abimon <eddyabby59@gmail.com>
2025-05-28 20:57:14 +0300

    combiled report on overspeed

--- resources/views/dashboard/trips/index.blade.php
+++ resources/views/dashboard/trips/index.blade.php
@@ -51,11 +51,12 @@
                                                         <thead>
                                                             <th>#</th>
                                                             <th>Start Time</th>
+                                                            <th>End Time</th>
+                                                            <th>Duration</th>
                                                             <th>Start Location</th>
+                                                            <th>End Location</th>
                                                             <th>Direction</th>
                                                             <th>Accuracy</th>
-                                                            <th>End Time</th>
-                                                            <th>End Location</th>
                                                             <th>Speed Limit</th>
                                                             <th>Highest Speed</th>
 
@@ -64,12 +65,13 @@
                                                             @foreach($trip->tripReport as $report)
                                                                 <tr>
                                                                     <td>{{ $loop->iteration }}</td>
-                                                                    <td>{{$report->start_time}}</td>
+                                                                    <td>{{date_format(date_create($report->start_time),'M jS Y H:i:s')}}</td>
+                                                                    <td>{{date_format(date_create($report->end_time),'M jS Y H:i:s')}}</td>
+                                                                    <td>{{ date_diff(date_create($report->start_time), date_create($report->end_time))->format('%h:%i:%s') }}</td>
                                                                     <td>{{$report->start_location}}</td>
+                                                                    <td>{{$report->end_location}}</td>
                                                                     <td>{{$report->direction}}</td>
                                                                     <td>{{$report->accuracy}}</td>
-                                                                    <td>{{$report->end_time}}</td>
-                                                                    <td>{{$report->end_location}}</td>
                                                                     <td>{{$report->speedLimit}}</td>
                                                                     <td>{{$report->highestSpeed}}</td>
                                                                 </tr>
