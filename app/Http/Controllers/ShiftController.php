5f5075af6f31c8f791d4416be62f53799c699b25
abimon <eddyabby59@gmail.com>
2025-06-24 15:58:29 +0300

    MPesa pay

--- app/Http/Controllers/ShiftController.php
+++ app/Http/Controllers/ShiftController.php
@@ -105,7 +105,6 @@ class ShiftController extends Controller
             'driver_id'=>request('driver_id'),
             'paid'=>false,
         ]);
-        return $shift;
         $driver = User::findOrFail(request('driver_id'));
         $contact = $driver->contact;
         // initiate mpesa payment
