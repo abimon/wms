4bde3439f7595f13ce3d537f3ff5a96d32d37fd9
abimon <eddyabby59@gmail.com>
2025-06-24 19:33:39 +0300

    amount

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-3">
                 <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
-                    <i class="fa fa-book fa-3x text-dark"></i>
+                    <i class="fa fa-dollar-sign fa-3x text-dark"></i>
                     <div class="ms-3">
                         <p class="mb-2">Total Revenue</p>
                         <h6 class="mb-0">{{App\Models\Mpesa::sum('TransAmount')}}</h6>
@@ -14,7 +14,7 @@
             </div>
             <div class="col-md-3">
                 <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
-                    <i class="fa fa-book fa-3x text-dark"></i>
+                    <i class="fa fa-tax fa-3x text-dark"></i>
                     <div class="ms-3">
                         <p class="mb-2">Trips Done Today</p>
                         <h6 class="mb-0">{{App\Models\Trip::where('created_at', '>=', Carbon\Carbon::today())->count()}}</h6>
@@ -23,7 +23,7 @@
             </div>
             <div class="col-md-3">
                 <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
-                    <i class="fa fa-book fa-3x text-dark"></i>
+                    <i class="fa fa-hotjar fa-3x text-dark"></i>
                     <div class="ms-3">
                         <p class="mb-2">Shifts Today</p>
                         <h6 class="mb-0">{{App\Models\Shift::where('created_at', '>=', Carbon\Carbon::today())->count()}}</h6>
@@ -32,7 +32,7 @@
             </div>
             <div class="col-md-3">
                 <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
-                    <i class="fa fa-book fa-3x text-dark"></i>
+                    <i class="fa fa-triangle-exclamation fa-3x text-dark"></i>
                     <div class="ms-3">
                         <p class="mb-2">Misconducts Today</p>
                         <h6 class="mb-0">{{(App\Models\TripReport::where('created_at', '>=', Carbon\Carbon::today())->count())+(App\Models\Report::where('created_at', '>=', Carbon\Carbon::today())->count())}}</h6>
