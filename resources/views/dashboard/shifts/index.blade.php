151395d7e01a3c97a4af10b06a3e50a909633498
abimon <eddyabby59@gmail.com>
2025-06-25 13:53:30 +0300

    shift report

---
+++ resources/views/dashboard/shifts/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app', ['title' => 'shifts'])
+@section('content')
+    <div class="container">
+        <div class="table table-responsive">
+            <table>
+                <thead>
+                    <th>#</th>
+                    <th>Number Plate</th>
+                    <th>Location</th>
+                    <th>Start Time</th>
+                    <th>Passenger</th>
+                    <th>Action</th>
+                    <th>Shift Reports</th>
+                </thead>
+                <tbody>
+                    @foreach($shifts as $shift)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $shift->vehicle_plate }}</td>
+                            <td>
+                                <a href="https://maps.google.com/maps?q={{$shift->start_location}}">{{ $shift->start_location }}</a>
+                                <br>{{ $shift->direction }}
+                            </td>
+                            <td>{{ $shift->created_at->format('F jS H:i:s') }}</td>
+                            <td>{{ $shift->passenger_contact }}</td>
+                            <td>
+                                <form action="{{ route('shifts.destroy', $shift->id) }}" method="POST" class="d-inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger">Delete</button>
+                                </form>
+                            </td>
+                            <td>
+                                <a href="{{ route('shifts.show', $shift->id)}}">
+                                    <button type="button" class="btn btn-primary">
+                                        Reports({{ $shift->shiftReport->count() }})
+                                    </button>
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/dashboard/shifts/show.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app', ['title' => 'Shift Details'])
+@section('content')
+    <div class="container">
+        <table class="table-bordered table-striped ">
+            <thead>
+                <th>#</th>
+                <th>Type</th>
+                <th>Start Time</th>
+                <th>End Time</th>
+                <!-- <th>Duration</th> -->
+                <th>Start Location</th>
+                <th>End Location</th>
+                <th>Direction</th>
+                <th>Accuracy</th>
+                <th>Speed Limit</th>
+                <th>Highest Speed</th>
+
+            </thead>
+            <tbody>
+                @foreach($shift->shiftreport as $report)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $report->type }}</td>
+                        <td>{{date_format(date_create($report->start_time), 'M jS Y H:i:s')}}</td>
+                        <td>{{date_format(date_create($report->end_time), 'M jS Y H:i:s')}}</td>
+                        <td></td>
+                        <td>{{$report->start_location}}</td>
+                        <td>{{$report->end_location}}</td>
+                        <td>{{$report->direction}}</td>
+                        <td>{{$report->accuracy}}</td>
+                        <td>{{$report->speedLimit}}</td>
+                        <td>{{$report->highestSpeed}}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
