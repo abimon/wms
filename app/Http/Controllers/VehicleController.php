35585d0592aca95556117d5814b553970f255453
abimon <eddyabby59@gmail.com>
2025-06-25 22:29:25 +0300

    vehicle details

---
+++ app/Http/Controllers/VehicleController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vehicle;
+use Illuminate\Http\Request;
+
+class VehicleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+            Vehicle::create([
+                'plate' => request('plate'),
+                'make' => request('make'),
+                'type' => request('type'),
+                'color' => request('color'),
+                'insurance' => request('insurance'),
+                'inspection' => request('inspection'),
+                'userId' => request('userId'),
+            ]);
+            return response()->json(['message' => 'Vehicle created successfully']);
+        } catch (\Throwable $th) {
+            return response()->json(['message'=> $th->getMessage()]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Vehicle $vehicle)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Vehicle $vehicle)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Vehicle $vehicle)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Vehicle $vehicle)
+    {
+        //
+    }
+}
