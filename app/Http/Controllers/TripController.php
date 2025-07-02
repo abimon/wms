2b5dfc1d60816d44e8f1f01a5314864ef9e34bf3
abimon <eddyabby59@gmail.com>
2025-07-02 23:41:04 +0300

    trip reports

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -57,7 +57,7 @@ class TripController extends Controller
         $data = [];
         foreach ($trips as $trip) {
             $time = $trip->tripReport->map(function ($t) {
-                return date_create($t->start_time)->getTimestamp() - date_create($t->end_time)->getTimestamp();
+                return date_create($t->end_time)->getTimestamp() - date_create($t->start_time)->getTimestamp();
             })->sum();
             array_push($data,['trip'=>$trip,'overspeeds'=>$trip->tripReport->count(),'total_time'=>$time]);
         }
